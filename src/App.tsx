a66f1fe69bb69e20e7c11c7c46b7c47371ba7630
Evgenii Khramkov <ewgeniux@gmail.com>
2020-11-18 09:58:04 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "@vkontakte/vkui/dist/vkui.css";
+import { AdaptivityProvider, View, Panel } from "@vkontakte/vkui";
+import CustomSelect from "@vkontakte/vkui/dist/components/CustomSelect/CustomSelect";
+import { CustomSelectProps } from "@vkontakte/vkui/dist/components/CustomSelect/CustomSelect";
+
+interface MyCustomSelectProps extends CustomSelectProps {
+  extraProperty: string;
+}
+
+export const MyCustomSelect = ({
+  extraProperty,
+  ...props
+}: MyCustomSelectProps) => {
+  return <CustomSelect {...props} />;
+};
+
+export default function App() {
+  return (
+    <AdaptivityProvider>
+      <View activePanel="panel1">
+        <Panel id="panel1">
+          <MyCustomSelect extraProperty="test" options={[]} />
+        </Panel>
+      </View>
+    </AdaptivityProvider>
+  );
+}
